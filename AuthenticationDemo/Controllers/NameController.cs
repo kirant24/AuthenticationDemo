7fa7dc489fc7e32748c36f98b0773db8dcfa7aae
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2021-12-15 09:08:09 +0530

    Controller
    Name Conroller created with authorization.

---
+++ AuthenticationDemo/Controllers/NameController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationDemo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jwtAuthenticationManager;
+
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/Name
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New Jersey", "New York" };
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCredential credential)
+        {
+            var token = jwtAuthenticationManager.Authenticate(credential.UserName, credential.Password);
+            return String.IsNullOrEmpty(token) ? Unauthorized() : Ok(token);
+        }
+    }
+}
---
+++ AuthenticationDemo/Controllers/UserCredential.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthenticationDemo.Controllers
+{
+    public class UserCredential
+    {
+        /// <summary>
+        /// User name.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
